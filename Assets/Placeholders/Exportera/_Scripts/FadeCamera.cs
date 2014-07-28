6200300611305d5e3cfa8489312dc35bfe7ae0e7
Kimphoe <kimandersson502@gmail.com>
2014-07-28 15:19:52 +0200

    placeholders

---
+++ Assets/Placeholders/Exportera/_Scripts/FadeCamera.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeCamera : MonoBehaviour {
+
+	public float _fadeSpeed = 1.0f;
+	public bool[] _fadeStates;
+
+	//public bool _fadeToWhite = false;
+
+	Color _fadeToColor;
+
+	Vector2 _screenSize;
+
+	void Awake(){
+		guiTexture.pixelInset = new Rect(-Screen.width/2f, -Screen.height/2f, Screen.width, Screen.height);
+		_fadeStates = new bool[2];
+		_fadeStates[0] = false;
+		_fadeStates[1] = false;
+
+		_screenSize = new Vector2(Screen.width, Screen.height);
+
+	}
+
+	void Update () {
+		if(!_fadeStates[0] && guiTexture.color.a != 0.0f)
+			StartFadeIn();
+		else if(_fadeStates[0] && guiTexture.color.a != 1.0f)
+			StartFadeOut(); 
+
+		if(_screenSize != new Vector2(Screen.width, Screen.height)){
+
+			_screenSize = new Vector2(Screen.width, Screen.height);
+			guiTexture.pixelInset = new Rect(-Screen.width/2f, -Screen.height/2f, Screen.width, Screen.height);
+
+		}
+
+		//_fadeToColor = (_fadeToWhite) ? Color.white : Color.black;
+
+	}
+
+	private void StartFadeIn(){
+
+		guiTexture.enabled = true;
+		FadeIn ();
+		
+		if(guiTexture.color.a < 0.05f){
+			_fadeStates[1] = false;
+			guiTexture.color = Color.clear;
+			guiTexture.enabled = false;
+		}
+	}
+
+	private void StartFadeOut(){
+
+		guiTexture.enabled = true;
+		FadeOut ();
+
+		if(guiTexture.color.a > 0.95f){
+			_fadeStates[1] = true;
+			guiTexture.color = _fadeToColor;
+		}
+	}
+	
+	private void FadeIn(){
+		guiTexture.color = Color.Lerp (guiTexture.color, Color.clear, _fadeSpeed * Time.deltaTime);
+	}
+	
+	public void FadeOut(){
+		guiTexture.color = Color.Lerp (guiTexture.color, _fadeToColor, _fadeSpeed * Time.deltaTime);
+	}
+
+	public void Fading(bool b){
+		_fadeStates[0] = b;
+	}
+
+	public bool FadeState(){
+		return _fadeStates[0] && _fadeStates[1];
+	}
+
+	public void SetFadeSpeed(float f){
+		_fadeSpeed = f;
+	}
+
+	public void SetFadeColor(Color c){
+
+		_fadeToColor = c;
+
+	}
+
+	public void SetFadeTexture(Texture tex){
+
+		GetComponent<GUITexture> ().texture = tex;
+
+	}
+}
---
+++ Assets/Placeholders/Exportera/_Scripts/FocusTarget.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FocusTarget : MonoBehaviour {
+    #region Public variables
+	public string FocusTag = "Focus";
+	public float rayCastLength = 1.0f;
+    #endregion
+
+    #region Private variables
+	Transform focusObject;
+	ThirdPersonCamera refToCamera;
+	Transform PlayerXForm;
+	CharacterController charController;
+	int layerMask;
+    #endregion
+
+    #region Structs 
+    #endregion
+
+    #region Enums
+    #endregion
+
+    #region Inits
+    //Called even if script component is not enabled
+    //best used for references between scripts and inits
+    void Awake() {
+		//grabbing reference to camera
+		refToCamera = Camera.main.GetComponent<ThirdPersonCamera>();
+		//grabbing a reference to the players character controller.
+		charController = GameObject.FindWithTag("Player").GetComponent<CharacterController>();
+		//ignore all but what we want to hit
+		layerMask = (1<<LayerMask.NameToLayer("Focus"));
+
+    }
+    
+    //Called if script component is enabled
+    void Start () {
+	    
+	}
+    #endregion
+
+    #region Update funtions
+    //every frame (1)
+	void Update () {
+		//grabbing the new transform pos from the player
+		PlayerXForm = GameObject.FindWithTag("CameraFollow").transform;
+
+
+	}
+
+    //after Update every frame (2)
+    void LateUpdate() {
+		//ray casting to check for focus targets
+		RaycastHit objectHit = new RaycastHit();
+		Vector3 p1 = PlayerXForm.position;
+		//we only want to hit object with the tag corrent layer mask.
+		layerMask = 1<<LayerMask.NameToLayer("Focus");
+		//raycasting several ray around there character to check for a focus target
+		if(Physics.SphereCast(p1,charController.height/2.0f,PlayerXForm.forward,out objectHit,rayCastLength,layerMask) || 
+		Physics.SphereCast(p1,charController.height/2.0f,-PlayerXForm.forward,out objectHit,rayCastLength,layerMask) ||
+		Physics.SphereCast(p1,charController.height/2.0f,PlayerXForm.right,out objectHit,rayCastLength,layerMask) ||
+		Physics.SphereCast(p1,charController.height/2.0f,-PlayerXForm.right,out objectHit,rayCastLength,layerMask) ||
+		Physics.SphereCast(p1,charController.height/2.0f,(PlayerXForm.forward+PlayerXForm.right).normalized,out objectHit,rayCastLength,layerMask) ||
+		Physics.SphereCast(p1,charController.height/2.0f,((-1*PlayerXForm.forward)+(-1*PlayerXForm.right).normalized ),out objectHit,rayCastLength,layerMask)) {
+
+			if(objectHit.transform.tag == FocusTag && Input.GetButtonDown("Interact")) {
+					refToCamera.setCameraState("Focus",objectHit.transform);
+			}
+			if(objectHit.transform.tag == FocusTag && Input.GetButtonUp("Interact")) {
+				refToCamera.setCameraState("Focus",objectHit.transform);
+				}
+			}
+
+    }
+
+    //physics updates (3) does not happen every frame
+    void FixedUpdate() {
+
+    }
+    #endregion
+
+    #region Private functions
+    #endregion
+
+    #region Public functions
+    #endregion
+}
